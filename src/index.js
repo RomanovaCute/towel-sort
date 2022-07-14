e3e1b6c5289e8aff691234f9bc49134473f64da1
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-14 10:19:17 +0500

    feat: add solution of towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let array = [];
+  
+  if(!Array.isArray(matrix) || !matrix.length){
+    return [];
+  } else {
+    matrix.map((item, i) => {
+      if(i % 2 === 0) {                    // если индекс массива в составе матрицы четный 0,2,4... 
+        array.push(...matrix[i])           // то добавляем входящие в него значения в массив array
+     } else {                              // иначе (индекс массив нечетный 1,3,5...)
+      array.push(...matrix[i].reverse())   // добавляем значения в обратном порядке в array
+     }
+    })
+    return array;
+  }  
+}      
